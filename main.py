54bd9dd7a06a34e435ab0ff5048e6a502c5742bf
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-13 11:28:27 +0900

    함수로 만들어보기

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ content = "공욱재"
 tag_jolib_open = angle_bracket[0] + tag_name + angle_bracket[1]
 tag_jolib_close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
 
-result = tag_jolib_open + content + tag_jolib_close
+
+## 매겨변수 설정하는 것이 많이 어렵다 -> 많이 해봐야 함
+def make_tag(isOpen):
+    if isOpen == True:
+        open = tag_jolib_open
+        return open 
+    else:
+        close = tag_jolib_close
+        return  close
+    
+
+result = make_tag(True) + content + make_tag(False)
+
+
+print(result)
