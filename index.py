c8f595bb282f49be04aa3fbd79950e02687d0939
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-13 16:50:35 +0900

    함수 만들깈ㅋ

--- index.py
+++ index.py
@@ -1,20 +1,27 @@
 # <html><head><title>제목</title></head><body><h1>내용</h1></body></html>
 
-open = "<"
-close = ">"
-slash = "/"
+# html = open + "html" + close 
+# head = open + "head" + close
+# tilte = open + "title" + close
+# body = open + "body" + close
+# h1 = open + "h1" + close
 
-#<html>
-html = open + "html" + close 
-head = open + "head" + close
-tilte = open + "title" + close
-body = open + "body" + close
-h1 = open + "h1" + close
-
-slash_html = open + slash + "html" + close
-slash_head = open + slash + "head" + close
-slash_title = open + slash + "title" + close
-slash_body = open + slash + "body" + close
-slash_h1 = open + slash + "h1" + close
+# slash_html = open + slash + "html" + close
+# slash_head = open + slash + "head" + close
+# slash_title = open + slash + "title" + close
+# slash_body = open + slash + "body" + close
+# slash_h1 = open + slash + "h1" + close
 
 
+def mark_up(content):
+    open = "<"
+    close = ">"
+    slash = "/"
+            
+    def open_mark_up(content):
+        keyword = open + content + close
+        print(keyword)
+
+    def close_mark_up(content):
+        keyword = open + slash + content + close
+        print(keyword)
