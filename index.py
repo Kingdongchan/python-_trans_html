2ce1e3ebfab45efafd48f64928a6fad3fa53aede
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-13 16:42:20 +0900

    html ->python 으로 만들어보기

---
+++ index.py
@@ -0,0 +1,20 @@
+# <html><head><title>제목</title></head><body><h1>내용</h1></body></html>
+
+open = "<"
+close = ">"
+slash = "/"
+
+#<html>
+html = open + "html" + close 
+head = open + "head" + close
+tilte = open + "title" + close
+body = open + "body" + close
+h1 = open + "h1" + close
+
+slash_html = open + slash + "html" + close
+slash_head = open + slash + "head" + close
+slash_title = open + slash + "title" + close
+slash_body = open + slash + "body" + close
+slash_h1 = open + slash + "h1" + close
+
+
