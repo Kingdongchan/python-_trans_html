b2416292c468edc6c6db11bb343281069682e8a5
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-13 11:34:41 +0900

    tag_name 매개변수 선언

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-
-tag_name = "h1"
-angle_bracket = ["<", ">"]
 content = "공욱재"
 
 ## 매겨변수 설정하는 것이 많이 어렵다 -> 많이 해봐야 함
-def make_tag(isOpen):
+def make_tag(isOpen, tag_name):
+    angle_bracket = ["<", ">"]
+    slash = "/"
+    
     if isOpen == True:
         open = angle_bracket[0] + tag_name + angle_bracket[1]
         return open 
     else:
-        close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
+        close = angle_bracket[0] + slash + tag_name + angle_bracket[1]
         return  close
     
 
-result = make_tag(True) + content + make_tag(False)
+result = make_tag(True, "h1") + content + make_tag(False, "h1")
 
 
 print(result)
