0d785de7fff0738aa4ec21882a464e74aaeb9f28
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-13 11:30:44 +0900

    변수 줄이기 -> 함수에 추가

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ tag_name = "h1"
 angle_bracket = ["<", ">"]
 content = "공욱재"
 
-
-tag_jolib_open = angle_bracket[0] + tag_name + angle_bracket[1]
-tag_jolib_close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
-
-
 ## 매겨변수 설정하는 것이 많이 어렵다 -> 많이 해봐야 함
 def make_tag(isOpen):
     if isOpen == True:
-        open = tag_jolib_open
+        open = angle_bracket[0] + tag_name + angle_bracket[1]
         return open 
     else:
-        close = tag_jolib_close
+        close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
         return  close
     
 
